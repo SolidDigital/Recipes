eeb7c2ffc222db4cf01cf3ba01886f06ceac2e27
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-07-30 00:44:13 -0700

    fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -16,59 +16,16 @@ function load() {
 
     app.router = router;
 
-    router.use('/', displayHomePage);
     router.use(displayRecipe);
 
     // 404 added as last piece of middleware
     router.use(pageNotFound);
 }
 
-function displayHomePage(req, res, next) {
-    saturateNodes(null, res, authToken.get(), next);
-}
-
-function saturateNodes(nodeId, res, token, next) {
-    var ghCore = app.ghCore;
-
-    ++pending;
-    console.log('PENDING ::::::::: ' + pending);
-    console.log('FETCHING ID:::::: ' + nodeId);
-    ghCore
-        .request(token)
-        .nodes.getChildren(nodeId)
-        .then(function(nodes) {
-            --pending;
-            console.log('\n\n\n\n\n\n');
-            console.log('pending', pending);
-            console.log('get chilren results for nodeId: ' + nodeId);
-            _.each(nodes, function(node) {
-                console.log('_ID :::::::::::: ' + node._id);
-            });
-
-            _.each(nodes, function(node) {
-                saturateNodes(node._id, res, token, next);
-            });
-
-            tryToSendResponse(pending, res);
-        })
-        .catch(next)
-        .fail(next);
-}
-
-function tryToSendResponse(pending, res) {
-    if (!pending) {
-        console.log('\n\n\n\n\n\n\n\n\n\nRENDER RENDER RENDER RENDER >>>>>>>>>>>>>');
-        res.render('recipe',{
-            title : 'Solid Recipes',
-            content : '<p>Test</p>',
-            node : []});
-    }
-}
-
 function displayRecipe(req, res, next) {
     var html = '',
         storedResponse,
-        slug = req.path,
+        slug = req.path == '/' ? '/home/home' : req.path,
         ghCore = app.ghCore,
         queryBuilder = app.ghCore.utilities.queryBuilder,
         findSlug = queryBuilder
